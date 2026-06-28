991eae7f6996d2198adabfefa7974fc5988026a5
Prathamesh Tiparkar <tiparkarprathamesh96@gmail.com>
2026-06-29 00:18:28 +0530

    Java 5 Programs

---
+++ Program085.java
@@ -0,0 +1,34 @@
+// Write a program to print the multiplication of a number
+
+import java.util.Scanner;
+
+class Logic
+{
+    void printTable(int iNo)
+    {
+        int iCnt = 0;
+
+        for(iCnt = 1; iCnt <= 10; iCnt++)
+        {
+            System.out.println(iNo * iCnt);
+        }
+    }
+}
+
+class Program085
+{
+    public static void main(String A[])
+    {
+        int iValue = 0;
+
+        Logic lobj = new Logic();
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter Number : ");
+        iValue = sobj.nextInt();
+
+        lobj.printTable(iValue);
+
+        sobj.close();
+    }
+}
