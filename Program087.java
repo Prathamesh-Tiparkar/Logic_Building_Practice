dee922c20089b12970e0197779f4eaaa38bd29fd
Prathamesh Tiparkar <tiparkarprathamesh96@gmail.com>
2026-06-29 00:19:21 +0530

    New Java 5 Programs

---
+++ Program087.java
@@ -0,0 +1,34 @@
+// Write a program to print all even numbers upto N
+
+import java.util.Scanner;
+
+class Logic
+{
+    void printEvenNumbers(int iNo)
+    {
+        int iCnt = 0;
+        for(iCnt = 2; iCnt <= iNo; iCnt++)
+        {
+            if(iCnt % 2 == 0)
+            {
+                System.out.println(iCnt);
+            }
+        }
+    }
+}
+
+class Program087
+{
+    public static void main(String A[]) 
+    {   
+        int iValue = 0;
+        Scanner sobj = new Scanner(System.in);
+        Logic lobj = new Logic();
+
+        System.out.println("Enter number : ");
+        iValue = sobj.nextInt();
+        
+        lobj.printEvenNumbers(iValue);
+        sobj.close();
+    }
+}
