850ca2d730375c3511bf54cfc9f09611bb042f0d
Prathamesh Tiparkar <tiparkarprathamesh96@gmail.com>
2026-07-01 00:39:48 +0530

    Solved Digits 5 Programs

---
+++ Program097.java
@@ -0,0 +1,35 @@
+// Write a program to print numbers from N down to 1 in reverse order
+
+import java.util.Scanner;
+
+class Logic
+{
+    void printReverse(int iNo)
+    {
+        int iCnt = 0;
+
+        for(iCnt = iNo; iCnt >= 1; iCnt--)
+        {
+            System.out.print(iCnt+"\t");
+        }
+
+        System.out.println();
+    }
+}
+
+class Program097
+{
+    public static void main(String A[])
+    {
+        int iValue = 0;
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter Number : ");
+        iValue = sobj.nextInt();
+
+        Logic lobj = new Logic();
+        lobj.printReverse(iValue);
+        sobj.close();
+    }
+}
+
