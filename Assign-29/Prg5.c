2d3d520333a89aa4b3e4850f00dd3fff57342616
Prathamesh Tiparkar <tiparkarprathamesh96@gmail.com>
2026-09-24 23:43:48 +0530

    Programming Assignment on String in C

---
+++ Assign-29/Prg5.c
@@ -0,0 +1,50 @@
+/*
+5. Accept division of student from user and depends on the division
+display exam timing. There are 4 divisions in school as A,B,C,D.
+Exam of division A at 7 AM, B at 8.30 AM, C at 9.20 AM and
+D at 10.30 AM. (Application should be case insensitive)
+
+Input  : C
+Output : Your exam at 9.20 AM
+
+Input  : d
+Output : Your exam at 10.30 AM
+*/
+
+#include<stdio.h>
+
+void DisplaySchedule(char chDiv)
+{
+    if(chDiv == 'A' || chDiv == 'a')
+    {
+        printf("Your exam at 7.00 AM\n");
+    }
+    else if(chDiv == 'B' || chDiv == 'b')
+    {
+        printf("Your exam at 8.30 AM\n");
+    }
+    else if(chDiv == 'C' || chDiv == 'c')
+    {
+        printf("Your exam at 9.20 AM\n");
+    }
+    else if(chDiv == 'D' || chDiv == 'd')
+    {
+        printf("Your exam at 10.30 AM\n");
+    }
+    else
+    {
+        printf("Invalid Division\n");
+    }
+}
+
+int main()
+{
+    char cValue = '\0';
+
+    printf("Enter your division : ");
+    scanf("%c",&cValue);
+
+    DisplaySchedule(cValue);
+
+    return 0;
+}
